13a49cb093705b4ce981ea3cb2abb6656e7e2fff
liver0377 <liverspecial@gmail.com>
2023-01-28 20:06:21 +0800

    update routing

--- http/http_conn.cc
+++ http/http_conn.cc
@@ -387,132 +387,234 @@ http_conn::HTTP_CODE http_conn::process_read()
 
 http_conn::HTTP_CODE http_conn::do_request()
 {
+//     strcpy(m_real_file, doc_root);
+//     int len = strlen(doc_root);
+//     //printf("m_url:%s\n", m_url);
+//     const char *p = strrchr(m_url, '/');
+// 
+//     //处理cgi
+//     if (cgi == 1 && (*(p + 1) == '2' || *(p + 1) == '3'))
+//     {
+// 
+//         //根据标志判断是登录检测还是注册检测
+//         char flag = m_url[1];
+// 
+//         char *m_url_real = (char *)malloc(sizeof(char) * 200);
+//         strcpy(m_url_real, "/");
+//         strcat(m_url_real, m_url + 2);
+//         strncpy(m_real_file + len, m_url_real, FILENAME_LEN - len - 1);
+//         free(m_url_real);
+// 
+//         //将用户名和密码提取出来
+//         //user=123&passwd=123
+//         char name[100], password[100];
+//         int i;
+//         for (i = 5; m_string[i] != '&'; ++i)
+//             name[i - 5] = m_string[i];
+//         name[i - 5] = '\0';
+// 
+//         int j = 0;
+//         for (i = i + 10; m_string[i] != '\0'; ++i, ++j)
+//             password[j] = m_string[i];
+//         password[j] = '\0';
+// 
+//         if (*(p + 1) == '3')
+//         {
+//             //如果是注册，先检测数据库中是否有重名的
+//             //没有重名的，进行增加数据
+//             char *sql_insert = (char *)malloc(sizeof(char) * 200);
+//             strcpy(sql_insert, "INSERT INTO user(username, passwd) VALUES(");
+//             strcat(sql_insert, "'");
+//             strcat(sql_insert, name);
+//             strcat(sql_insert, "', '");
+//             strcat(sql_insert, password);
+//             strcat(sql_insert, "')");
+// 
+//             if (users.find(name) == users.end())
+//             {
+//                 m_lock.lock();
+//                 int res = mysql_query(mysql, sql_insert);
+//                 users.insert(pair<string, string>(name, password));
+//                 m_lock.unlock();
+// 
+//                 if (!res)
+//                     strcpy(m_url, "/log.html");
+//                 else
+//                     strcpy(m_url, "/registerError.html");
+//             }
+//             else
+//                 strcpy(m_url, "/registerError.html");
+//         }
+//         //如果是登录，直接判断
+//         //若浏览器端输入的用户名和密码在表中可以查找到，返回1，否则返回0
+//         else if (*(p + 1) == '2')
+//         {
+//             if (users.find(name) != users.end() && users[name] == password)
+//                 strcpy(m_url, "/welcome.html");
+//             else
+//                 strcpy(m_url, "/logError.html");
+//         }
+//     }
+// 
+//     if (*(p + 1) == '0')
+//     {
+//         char *m_url_real = (char *)malloc(sizeof(char) * 200);
+//         strcpy(m_url_real, "/register.html");
+//         strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+// 
+//         free(m_url_real);
+//     }
+//     else if (*(p + 1) == '1')
+//     {
+//         char *m_url_real = (char *)malloc(sizeof(char) * 200);
+//         strcpy(m_url_real, "/log.html");
+//         strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+// 
+//         free(m_url_real);
+//     }
+//     else if (*(p + 1) == '5')
+//     {
+//         char *m_url_real = (char *)malloc(sizeof(char) * 200);
+//         strcpy(m_url_real, "/picture.html");
+//         strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+// 
+//         free(m_url_real);
+//     }
+//     else if (*(p + 1) == '6')
+//     {
+//         char *m_url_real = (char *)malloc(sizeof(char) * 200);
+//         strcpy(m_url_real, "/video.html");
+//         strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+// 
+//         free(m_url_real);
+//     }
+//     else if (*(p + 1) == '7')
+//     {
+//         char *m_url_real = (char *)malloc(sizeof(char) * 200);
+//         strcpy(m_url_real, "/fans.html");
+//         strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+// 
+//         free(m_url_real);
+//     }
+//     else
+//         strncpy(m_real_file + len, m_url, FILENAME_LEN - len - 1);
+// 
+//     if (stat(m_real_file, &m_file_stat) < 0)
+//         return NO_RESOURCE;
+// 
+//     if (!(m_file_stat.st_mode & S_IROTH))
+//         return FORBIDDEN_REQUEST;
+// 
+//     if (S_ISDIR(m_file_stat.st_mode))
+//         return BAD_REQUEST;
+// 
+//     int fd = open(m_real_file, O_RDONLY);
+//     m_file_address = (char *)mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+//     close(fd);
+//     return FILE_REQUEST;
     strcpy(m_real_file, doc_root);
-    int len = strlen(doc_root);
-    //printf("m_url:%s\n", m_url);
-    const char *p = strrchr(m_url, '/');
 
-    //处理cgi
-    if (cgi == 1 && (*(p + 1) == '2' || *(p + 1) == '3'))
-    {
+    int type = -1;  // 0: login, 1: register, 2: file request
+    if (strcmp(m_url, "/login") == 0) {
+        type = 0;
+    } else if (strcmp(m_url, "/register") == 0) {
+        type = 1;
+    } else {
+        type = 2;
+    }
 
-        //根据标志判断是登录检测还是注册检测
-        char flag = m_url[1];
+    char url[100] = {0};
+    strcpy(url, m_url);
 
-        char *m_url_real = (char *)malloc(sizeof(char) * 200);
-        strcpy(m_url_real, "/");
-        strcat(m_url_real, m_url + 2);
-        strncpy(m_real_file + len, m_url_real, FILENAME_LEN - len - 1);
-        free(m_url_real);
+    // std::cout << "type: " << type << std::endl;
+    // std::cout << "m_body: " << m_body << std::endl;
 
-        //将用户名和密码提取出来
-        //user=123&passwd=123
-        char name[100], password[100];
+    if (type == 0 || type == 1) {
+        char name[100];
+        char passwd[100];
+
         int i;
-        for (i = 5; m_string[i] != '&'; ++i)
-            name[i - 5] = m_string[i];
+        for (i = 5; m_string[i] != '&'; ++i) {
+        name[i - 5] = m_string[i];
+        }
         name[i - 5] = '\0';
 
         int j = 0;
-        for (i = i + 10; m_string[i] != '\0'; ++i, ++j)
-            password[j] = m_string[i];
-        password[j] = '\0';
+        for (i = i + 10; m_string[i] != '\0'; ++i, ++j) {
+        passwd[j] = m_string[i];
+        }
+        passwd[j] = '\0';
 
-        if (*(p + 1) == '3')
-        {
-            //如果是注册，先检测数据库中是否有重名的
-            //没有重名的，进行增加数据
-            char *sql_insert = (char *)malloc(sizeof(char) * 200);
-            strcpy(sql_insert, "INSERT INTO user(username, passwd) VALUES(");
-            strcat(sql_insert, "'");
-            strcat(sql_insert, name);
-            strcat(sql_insert, "', '");
-            strcat(sql_insert, password);
-            strcat(sql_insert, "')");
+        // std::cout << "user: " << name << "passwd: " << passwd << std::endl;
+        // 登录
+        if (type == 0) {
+        // std::cout << "m_users_map size: " << m_users_map->size() <<
+        // std::endl;
+        if (users.find(name) != users.end() &&
+            users[name] == passwd) {
+            strcpy(url, "/welcome.html");
+        } else {
+            // std::cout << "log error" << std::endl;
+            strcpy(url, "/logError.html");
+        }
+        }
 
-            if (users.find(name) == users.end())
-            {
-                m_lock.lock();
-                int res = mysql_query(mysql, sql_insert);
-                users.insert(pair<string, string>(name, password));
-                m_lock.unlock();
+        // 注册
+        if (type == 1) {
+        char *sql_insert = (char *)malloc(sizeof(char) * 200);
+        strcpy(sql_insert, "INSERT INTO user(username, passwd) VALUES(");
+        strcat(sql_insert, "'");
+        strcat(sql_insert, name);
+        strcat(sql_insert, "', '");
+        strcat(sql_insert, passwd);
+        strcat(sql_insert, "')");
 
-                if (!res)
-                    strcpy(m_url, "/log.html");
-                else
-                    strcpy(m_url, "/registerError.html");
+        if (users.find(name) == users.end()) {
+            int res = mysql_query(mysql, sql_insert);
+            m_lock.lock();
+            users.insert(
+                std::pair<std::string, std::string>(name, passwd));
+            m_lock.unlock();
+
+            if (!res) {
+                strcpy(url, "/log.html");
+            } else {
+                strcpy(url, "/registerError.html");
             }
-            else
-                strcpy(m_url, "/registerError.html");
+        } else {
+            strcpy(url, "/registerError.html");
         }
-        //如果是登录，直接判断
-        //若浏览器端输入的用户名和密码在表中可以查找到，返回1，否则返回0
-        else if (*(p + 1) == '2')
-        {
-            if (users.find(name) != users.end() && users[name] == password)
-                strcpy(m_url, "/welcome.html");
-            else
-                strcpy(m_url, "/logError.html");
         }
     }
 
-    if (*(p + 1) == '0')
-    {
-        char *m_url_real = (char *)malloc(sizeof(char) * 200);
-        strcpy(m_url_real, "/register.html");
-        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+    int len = strlen(doc_root);
 
-        free(m_url_real);
-    }
-    else if (*(p + 1) == '1')
-    {
-        char *m_url_real = (char *)malloc(sizeof(char) * 200);
-        strcpy(m_url_real, "/log.html");
-        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+    strncpy(m_real_file + len, url, FILENAME_LEN - len - 1);
+    // std::cout << "m_real_file" << m_real_file << std::endl;
 
-        free(m_url_real);
-    }
-    else if (*(p + 1) == '5')
-    {
-        char *m_url_real = (char *)malloc(sizeof(char) * 200);
-        strcpy(m_url_real, "/picture.html");
-        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
-
-        free(m_url_real);
-    }
-    else if (*(p + 1) == '6')
-    {
-        char *m_url_real = (char *)malloc(sizeof(char) * 200);
-        strcpy(m_url_real, "/video.html");
-        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
-
-        free(m_url_real);
-    }
-    else if (*(p + 1) == '7')
-    {
-        char *m_url_real = (char *)malloc(sizeof(char) * 200);
-        strcpy(m_url_real, "/fans.html");
-        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
-
-        free(m_url_real);
-    }
-    else
-        strncpy(m_real_file + len, m_url, FILENAME_LEN - len - 1);
-
-    if (stat(m_real_file, &m_file_stat) < 0)
+    if (stat(m_real_file, &m_file_stat) < 0) {
         return NO_RESOURCE;
+    }
 
-    if (!(m_file_stat.st_mode & S_IROTH))
+    if (!(m_file_stat.st_mode & S_IROTH)) {
         return FORBIDDEN_REQUEST;
+    }
 
-    if (S_ISDIR(m_file_stat.st_mode))
+    if (S_ISDIR(m_file_stat.st_mode)) {
         return BAD_REQUEST;
+    }
 
     int fd = open(m_real_file, O_RDONLY);
-    m_file_address = (char *)mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    m_file_address =
+        (char *)mmap(NULL, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    assert(m_file_address != MAP_FAILED);
     close(fd);
+
+    // std::cout << "url : " << url << " request file: " << m_real_file <<
+    // std::endl; std::cout << "do_request() over" << std::endl;
     return FILE_REQUEST;
 }
+
 void http_conn::unmap()
 {
     if (m_file_address)
